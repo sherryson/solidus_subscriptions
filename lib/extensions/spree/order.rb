5df914a13d75e173a86a1db5b7424f2253fd1b5e
Bryan Mahoney <bryan@goDynamo.com>
2013-06-26 18:38:45 -0400

    A newly created subscription should be in an active state

--- lib/extensions/spree/order.rb
+++ lib/extensions/spree/order.rb
@@ -23,6 +23,7 @@ module SpreeSubscriptions
           attrs = {
             ship_address_id: ship_address.id,
             user_id: user.id,
+            state: 'active',
             interval: subscription_interval
           }
 
