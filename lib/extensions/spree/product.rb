ba932b49875ab48ffc5571ed357f05155e3327e3
Bryan Mahoney <bryan@goDynamo.com>
2013-11-09 11:04:02 -0500

    Adds new scopes to variants

--- lib/extensions/spree/product.rb
+++ lib/extensions/spree/product.rb
@@ -4,6 +4,19 @@ module SpreeSubscriptions
       module Product
         extend ActiveSupport::Concern
 
+        module ClassMethods
+          def subscribable
+            ids = ::Spree::Variant.with_frequency.map(&:product_id).uniq
+            where('spree_products.id IN (?)', ids).select { |p| p.subscribable? }
+          end
+
+          def prepayable
+            ids = ::Spree::Variant.with_frequency.map(&:product_id).uniq
+            where('spree_products.id IN (?)', ids).select { |p| p.subscribable? }
+          end
+        end
+
+
         def subscribable_option_values
           variants.collect(&:option_values).flatten.select do |ov|
            ov.name.to_i > 1 && ov.option_type.name == frequency_option_type
@@ -17,7 +30,7 @@ module SpreeSubscriptions
         def frequency_option_type
           ::Spree::OptionType.find_by_name('frequency').name
         end
-         
+
         def subscribable_variants
           @subscribable_variants ||= variants.select do |v|
             frequencies = v.option_values.joins(:option_type).where('spree_option_types.name = ?', 'frequency')
---
+++ lib/extensions/spree/variant.rb
@@ -0,0 +1,33 @@
+module SpreeSubscriptions
+  module Extensions
+    module Spree
+      module Variant
+        extend ActiveSupport::Concern
+
+        def frequency
+          option_values.of_option_type('frequency').first
+        end
+
+        module ClassMethods
+          def with_frequency(*option_values)
+
+            relation = joins(:option_values => :option_type).where('spree_option_types.name' => 'frequency' )
+
+            option_values_conditions = option_values.each do |option_value|
+              option_value_conditions = case option_value
+              when OptionValue then { "spree_option_values.name" => option_value.name }
+              when String      then { "spree_option_values.name" => option_value }
+              else                  { "spree_option_values.id"   => option_value }
+              end
+              relation = relation.where(option_value_conditions)
+            end
+
+            relation
+          end
+        end
+      end
+    end
+  end
+end
+
+::Spree::Variant.send(:include, SpreeSubscriptions::Extensions::Spree::Variant)
