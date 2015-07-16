8dca6466a38a3d6766774ba00b70338d4ea452ec
David Ang <davidang09@gmail.com>
2015-07-17 00:28:16 +0800

    correct way to create shipment uses the helper methods transition_order_from_cart_to_payment! and transition_order_from_payment_to_complete! instead of manually doing it
    syntax fix for shipping the order

--- spec/support/order_macros.rb
+++ spec/support/order_macros.rb
@@ -1,35 +1,28 @@
-module OrderMacros
+module OrderMacros  
+  include SubscriptionTransitions
 
   def create_completed_prepaid_subscription_order
     order_factory
     @order.line_items << FactoryGirl.create(:line_item, variant: @prepaid_variant)
-    @order.shipping_method = Spree::ShippingMethod.first
-    @order.create_shipment!
-    @order.reload
-    @order.payments.create!({source: @card, payment_method: @gateway, amount: @order.total, state: 'completed'}, without_protection: true)
-    @order.state = 'complete'
-    @order.shipment.state = 'ready'
-    @order.shipment.ship!
-    @order.shipment_state = 'shipped'
-    @order.payment_state = 'paid'
-    @order.finalize!
-    @order.save
+    @order.create_proposed_shipments        
+    @order.payments.create!({source: @card, payment_method: @gateway, amount: @order.total})    
+    
+    transition_order_from_cart_to_payment!(@order)
+    transition_order_from_payment_to_complete!(@order)    
+
+    ship_order(@order)        
   end
 
   def create_completed_subscription_order
     order_factory
     @order.line_items << @line_items
-    @order.shipping_method = Spree::ShippingMethod.first
-    @order.create_shipment!
-    @order.reload
-    @order.payments.create!({source: @card, payment_method: @gateway, amount: @order.total, state: 'completed'}, without_protection: true)
-    @order.state = 'complete'
-    @order.shipment.state = 'ready'
-    @order.shipment.ship!
-    @order.shipment_state = 'shipped'
-    @order.payment_state = 'paid'
-    @order.finalize!
-    @order.save
+    @order.create_proposed_shipments    
+    @order.payments.create!({source: @card, payment_method: @gateway, amount: @order.total})
+    
+    transition_order_from_cart_to_payment!(@order)
+    transition_order_from_payment_to_complete!(@order)    
+    
+    ship_order(@order)
   end
 
   def order_factory
@@ -37,17 +30,23 @@ module OrderMacros
     @state = create(:state)
     @country = @state.country
     country_zone.members.create(:zoneable => @country)
-    @shipping_method = create(:shipping_method, :zone => country_zone)
+    @shipping_method = create(:shipping_method)
 
     @user = stub_model(Spree::User, email: "spree@example.com")
     @order = FactoryGirl.create(:order, ship_address: FactoryGirl.create(:address), bill_address: FactoryGirl.create(:address))
-
+    
     @line_items = [
-      FactoryGirl.create(:line_item),
+      # FactoryGirl.create(:line_item),
       FactoryGirl.create(:line_item, variant: @subscribable_variant)
     ]
 
-    @gateway = Spree::Gateway::Bogus.create!({environment: 'test', active: true, name: 'Credit Card'}, without_protection: true)
+    @gateway = Spree::Gateway::Bogus.create!({environment: 'test', active: true, name: 'Credit Card'})
     @card = FactoryGirl.create(:credit_card)
+  end
+
+  def ship_order(order)
+    shipment = order.shipments.first 
+    shipment.state = 'ready'    
+    shipment.ship!    
   end
 end
