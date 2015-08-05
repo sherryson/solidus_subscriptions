089ed4e355fdf45322c16404dc71a9e71205cf21
Bryan Mahoney <bryan@goDynamo.com>
2015-08-05 09:39:04 -0400

    Don't require lib/extension files

--- lib/spree_subscriptions/engine.rb
+++ lib/spree_subscriptions/engine.rb
@@ -15,11 +15,6 @@ module SpreeSubscriptions
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-
-      Dir.glob(File.join(File.dirname(__FILE__), "../../lib/extensions/**/*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-
     end
 
     config.to_prepare &method(:activate).to_proc
