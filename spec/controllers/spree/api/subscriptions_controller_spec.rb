4ef9650d4a2cd86d71878024a845b52bd455ed0f
David Ang <davidang09@gmail.com>
2015-09-12 07:21:36 +0800

    api spec for skipping orders, cancelling, pausing and resuming

--- spec/controllers/spree/api/subscriptions_controller_spec.rb
+++ spec/controllers/spree/api/subscriptions_controller_spec.rb
@@ -15,23 +15,65 @@ module Spree
     before do
       stub_authentication!
       setup_subscribable_products
+
+      create_completed_subscription_order
+      @subscription = Spree::Subscription.last
+      @subscription.update_column(:user_id, current_api_user.id)
     end
 
     it "restricts access to subscriptions" do
-      create_completed_subscription_order
-      @subscription = Spree::Subscription.last
+      @subscription.update_column(:user_id, 999)
       api_get :show, id: @subscription.id
       expect(json_response[:id]).to be nil
     end
 
     it "should allow the owner of the subscription to access it" do
-      create_completed_subscription_order
-      @subscription = Spree::Subscription.last
-      @subscription.user = current_api_user
-      @subscription.save
       api_get :show, id: @subscription.id
       expect(json_response[:id]).to be @subscription.id
     end
 
+    context "skipping" do
+      it "should skip next order" do
+        @subscription.next_shipment_date.to_date.should == 2.weeks.from_now.to_date
+
+        api_get :skip_next_order, id: @subscription.id
+        @subscription.next_shipment_date.to_date.should == 4.weeks.from_now.to_date
+      end
+
+      it "should be able to undo a skip next order" do
+        @subscription.skip_next_order
+        @subscription.next_shipment_date.to_date.should == 4.weeks.from_now.to_date
+
+        api_get :undo_skip_next_order, id: @subscription.id
+        @subscription.next_shipment_date.to_date.should == 2.weeks.from_now.to_date
+      end
+    end
+
+    it "should be able to cancel a subscription" do
+      api_put :cancel, id: @subscription.id
+      @subscription.reload
+
+      expect(@subscription.state).to eq 'cancelled'
+      expect(@subscription.can_renew?).to be false
+    end
+
+    it "should be able to pause a subscription" do
+      api_put :pause, id: @subscription.id
+      @subscription.reload
+
+      expect(@subscription.state).to eq 'paused'
+      expect(@subscription.can_renew?).to be false
+    end
+
+    it "should be able to resume after pausing" do
+      @subscription.pause
+      expect(@subscription.state).to eq 'paused'
+
+      api_put :resume, id: @subscription.id
+      @subscription.reload
+
+      expect(@subscription.state).to eq 'active'
+      expect(@subscription.can_renew?).to be true
+    end
   end
 end
