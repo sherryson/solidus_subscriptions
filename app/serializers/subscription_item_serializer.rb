2c2f79b68bbe2e1f021ed10dc601d753b40cfa86
David Ang <davidang09@gmail.com>
2015-10-06 23:40:15 +0800

    include price in the subscription item serializer

--- app/serializers/subscription_item_serializer.rb
+++ app/serializers/subscription_item_serializer.rb
@@ -3,7 +3,7 @@ class SubscriptionItemSerializer < ActiveModel::Serializer
   cached
   delegate :cache_key, to: :object
 
-  attributes :id, :quantity
+  attributes :id, :quantity, :price
 
   attributes :variant
 
