6dc94ce9ad7aa0813239c1f6e77d5876c4907c59
David Ang <davidang09@gmail.com>
2016-05-16 20:00:39 +0800

    temporary removing link to log entries

--- app/overrides/add_log_entries_to_admin_edit_user.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/users/_sidebar",
-  name: "admin_user_sidebar_log_entries",
-  insert_bottom: "[data-hook='admin_user_tab_options']",
-  text: "<li><%= link_to 'Log Entries', admin_log_entries_path(source_type: 'Spree::User', source_id: @user.id) %></li>",
-)
