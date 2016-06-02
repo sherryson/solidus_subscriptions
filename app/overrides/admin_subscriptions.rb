feaabc9673e8787a28d0a5a33c21b2849c4f0acb
Sherry Son <son.sherrysoomin92@gmail.com>
2016-06-02 15:31:15 -0400

    added button to link to new page to adjust skus of subscriptions

--- app/overrides/admin_subscriptions.rb
+++ app/overrides/admin_subscriptions.rb
@@ -8,3 +8,9 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_content_header",
                      :insert_top => "[data-hook='toolbar']>ul",
                      :text => "<li><%= link_to('Failed Renewals', failures_admin_subscriptions_path, class: 'button fa') %></li>"
                      )
+
+Deface::Override.new(:virtual_path => "spree/admin/shared/_content_header",
+                     :name => "admin_subscriptions_adjust_skus",
+                     :insert_top => "[data-hook='toolbar']>ul",
+                     :text => "<li><%= link_to('Adjust SKUs', adjust_sku_admin_subscriptions_path, class: 'button fa') %></li>"
+                     )
