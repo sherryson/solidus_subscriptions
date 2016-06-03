c9949b3d537319286d1402b7a0c83e087e3426e8
Sherry Son <son.sherrysoomin92@gmail.com>
2016-06-03 16:24:02 -0400

    display refresh icon next to subscription tab

--- app/overrides/admin_subscriptions.rb
+++ app/overrides/admin_subscriptions.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "subscriptions_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:subscriptions) %>")
+                     :text => "<%= tab(:subscriptions, icon: 'refresh') %>")
 
 Deface::Override.new(:virtual_path => "spree/admin/shared/_content_header",
                      :name => "admin_subscriptions_failures",
